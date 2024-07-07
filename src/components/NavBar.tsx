d0fd35dff679ecfc245bb203d84b3e620fd58f9e
Abdulmajid <ermatovabdumajid23@gmail.com>
2024-07-07 11:07:47 +0500

    Add navbar and dark mode

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src="" alt="logo" sizes="60" />
+      <Text>NavBar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
